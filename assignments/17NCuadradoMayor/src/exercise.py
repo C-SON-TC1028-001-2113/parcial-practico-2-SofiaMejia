56af6b702b437cbd928d0f7aa75f7992c6817676
SofiaMejia <A01252540@itesm.mx>
2021-09-10 17:10:45 +0000

    17 n cuadrado mayor

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,9 +1,15 @@
 
 
 def main():
-    numero = int(input("Escribe un numero : "))
+    
     #escribe tu código abajo de esta línea
+    numero = int(input("Escribe un numero : "))
+    ans = 1
 
+    while ans**2<=numero:
+        ans += 1
+    
+    print(ans)
 
 if __name__=='__main__':
     main()
